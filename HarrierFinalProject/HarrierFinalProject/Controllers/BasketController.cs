546ec89a3a26b51904f03fc8598834e44f1db33e
ilhambinyetov <binyetov631@gmail.com>
2021-12-07 19:39:25 +0400

    Basket Controller And Index View Created

---
+++ HarrierFinalProject/HarrierFinalProject/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+﻿using HarrierFinalProject.Data;
+using HarrierFinalProject.Data.Models;
+using HarrierFinalProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrierFinalProject.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            List<Car> cars = _context.Cars.Include(c => c.CarImages).Include(c => c.Brand).Include(c => c.Model).ToList();
+            List<Advertising> advertisings = _context.Advertisings.ToList();
+
+
+            BasketViewModel basketVM = new BasketViewModel()
+            {
+                Advertisings = advertisings,
+                Cars = cars
+            };
+
+            return View(basketVM);
+        }
+    }
+}
---
+++ HarrierFinalProject/HarrierFinalProject/ViewModels/BasketViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HarrierFinalProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrierFinalProject.ViewModels
+{
+    public class BasketViewModel
+    {
+        public List<Car> Cars { get; set; }
+        public List<Advertising> Advertisings { get; set; }
+    }
+}
