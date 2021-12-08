8c5d434269b8ca489380404abec6a455f27afa19
ilhambinyetov <binyetov631@gmail.com>
2021-12-09 02:08:55 +0400

    Model Controller of Admin Panel Created

---
+++ HarrierFinalProject/HarrierFinalProject/Areas/Manage/Controllers/ModelController.cs
@@ -0,0 +1,75 @@
+﻿using HarrierFinalProject.Areas.Manage.ViewModels;
+using HarrierFinalProject.Data;
+using HarrierFinalProject.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrierFinalProject.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ModelController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ModelController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Model> models = _context.Models.ToList();
+
+            ModelViewModel modelVM = new ModelViewModel()
+            {
+                Models = models
+            };
+
+            return View(modelVM);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<Brand> brands = _context.Brands.ToList();
+
+            ModelViewModel modelVM = new ModelViewModel()
+            {
+                Brands = brands
+            };
+
+            return View(modelVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ModelViewModel modelVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Model model = new Model()
+            {
+                Name = modelVM.Name,
+                BrandId = modelVM.BrandId
+            };
+
+            _context.Models.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public IActionResult  Edit(ModelViewModel modelVM)
+
+        {
+
+
+            return View(modelVM);
+        }
+    }
+}
---
+++ HarrierFinalProject/HarrierFinalProject/Areas/Manage/ViewModels/ModelViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HarrierFinalProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrierFinalProject.Areas.Manage.ViewModels
+{
+    public class ModelViewModel
+    {
+        public List<Model> Models { get; set; }
+        public List<Brand> Brands { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+    }
+
+}
