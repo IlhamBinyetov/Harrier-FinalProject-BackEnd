e005d9b549f200209fbf7900f2e269f96e44f5cf
ilhambinyetov <binyetov631@gmail.com>
2021-12-27 16:42:46 +0400

    Comment Controller of Admin Panel Created and View is ready

---
+++ HarrierFinalProject/HarrierFinalProject/Areas/Manage/Controllers/CommentController.cs
@@ -0,0 +1,60 @@
+﻿using HarrierFinalProject.Areas.Manage.ViewModels;
+using HarrierFinalProject.Data;
+using HarrierFinalProject.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrierFinalProject.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class CommentController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public CommentController(AppDbContext context, UserManager<AppUser> userManager )
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            List<Comment> Comments = _context.Comments.Include(x=>x.AppUser).ToList();
+
+            CommentsViewModel commentVM = new CommentsViewModel()
+            {
+                Comments = Comments
+            };
+
+            return View(commentVM);
+        }
+
+
+        public IActionResult DeleteFetch(int id)
+        {
+            Comment comment = _context.Comments.FirstOrDefault(x => x.Id == id);
+
+            if (comment == null) return Json(new { status = 404 });
+
+            try
+            {
+                _context.Comments.Remove(comment);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(new { status = 500 });
+            }
+
+
+            return Json(new { status = 200 });
+
+
+        }
+    }
+}
---
+++ HarrierFinalProject/HarrierFinalProject/Areas/Manage/ViewModels/CommentsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HarrierFinalProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrierFinalProject.Areas.Manage.ViewModels
+{
+    public class CommentsViewModel
+    {
+        public Comment Comment { get; set; }
+        public List<Comment> Comments { get; set; }
+        public int CarId { get; set; }
+        public string Description { get; set; }
+    }
+}
